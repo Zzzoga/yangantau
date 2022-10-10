35f5c66963442d1978950a311ec48e19f5b9a0f5
Zzzoga <105968987+Zzzoga@users.noreply.github.com>
2022-10-10 15:04:20 +0500

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,50 +1,22 @@
-$(function() {
+// Smooth Scroll Script
 
-	//SVG Fallback
-	if(!Modernizr.svg) {
-		$("img[src*='svg']").attr("src", function() {
-			return $(this).attr("src").replace(".svg", ".png");
-		});
-	};
+SmoothScroll({
+    animationTime    : 1000,
+    stepSize         : 50,
+    
+    accelerationDelta : 30,  
+    accelerationMax   : 2,   
 
-	//E-mail Ajax Send
-	//Documentation & Example: https://github.com/agragregra/uniMail
-	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
+    keyboardSupport   : true,  
+    arrowScroll       : 50,
 
-	//Chrome Smooth Scroll
-	try {
-		$.browserSelector();
-		if($("html").hasClass("chrome")) {
-			$.smoothScroll();
-		}
-	} catch(err) {
+    pulseAlgorithm   : true,
+    pulseScale       : 4,
+    pulseNormalize   : 1,
 
-	};
+    touchpadSupport   : true,
+})
 
-	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
-	
-});
-
-$(window).load(function() {
-
-	$(".loader_inner").fadeOut();
-	$(".loader").delay(400).fadeOut("slow");
-
-});
 
 // SLICK SLIDER ON HEADER
 $('.slider').slick({
@@ -57,18 +29,24 @@ $('.slider').slick({
 	autoplaySpeed: 5000
   });
 
+// GSAP Animation Full-width block with infoblock
+
 const tl = gsap.timeline();
 
-tl.to('.one', {x: '-600px', duration: 3}, 0)
-tl.to('.two', {x: '-600px', duration: 3}, 0)
-tl.to('.three', {x: '-600px', duration: 3}, 0)
-tl.to('.five', {x: '600px', duration: 3}, 0)
-tl.to('.six', {x: '600px', duration: 3}, 0)
-tl.to('.seven', {x: '600px', duration: 3}, 0)
-tl.to('.four img', {width: '100vw', top: '0', duration: 3}, 0)
-tl.to('.overlay-img', {width: '100vw', opacity: 1, top: 0, duration: 3}, 0)
-tl.to('.first-item', {top: '180vh', duration: 3}, 1)
-tl.to('.second-item', {top: '190vh', duration: 4}, 1)
+tl.to('.one', {x: '-1200px', duration: 3}, 0)
+tl.to('.two', {x: '-900px', duration: 3}, 0)
+tl.to('.three', {x: '-900px', duration: 3}, 0)
+tl.to('.five', {x: '900px', duration: 3}, 0)
+tl.to('.six', {x: '900px', duration: 3}, 0)
+tl.to('.seven', {x: '1200px', duration: 3}, 0)
+tl.to('.four img', {width: '130vw', top: '0', duration: 3}, 0)
+tl.fromTo('.overlay-img', 
+	{width: '800px', height: '529.34px', opacity: 0, top: '55px', duration: 3}, 
+	{width: '2496px', height: '1999.91px', opacity: 1, top: 0, duration: 3}, 
+0)
+
+tl.to('.infoblock', {top: '70vh', duration: 3}, 1)
+
 
 ScrollTrigger.create({
 	animation: tl,
@@ -80,6 +58,46 @@ ScrollTrigger.create({
 	pin: true
 });
 
+// Higher blocks + parallax on scroll action
+
+const tlSecond = gsap.timeline();
+
+tlSecond.to('.first__vac', {y: '30px', duration: 1}, 0)
+tlSecond.to('.second__vac', {y: '170px', duration: 1}, 0)
+tlSecond.to('.third__vac', {y: '30px', duration: 1}, 0)
+
+tlSecond.to('.v-item-img-first img', {y: '-70px', duration: 1}, 0)
+tlSecond.to('.v-item-img-second img', {y: '-70px', duration: 1}, 0)
+tlSecond.to('.v-item-img-third img', {y: '-70px', duration: 1}, 0)
+
+ScrollTrigger.create({
+	animation: tlSecond,
+	trigger: '.scroll-container',
+	markers: false,
+	start: 'top top',
+	end: 'center top',
+	scrub: 1,
+	pin: true
+});
 
 
+// Change background img on fixed blocks when scrolling
+const medicalBackground = document.querySelector(".medical__inner")
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+				if ($(window).scrollTop() > 8019 && $(window).scrollTop() < 8917){
+					medicalBackground.style.background = 'url(../img/medical-base-1.png) no-repeat center top'
+					medicalBackground.style.backgroundAttachment = 'fixed'
+				} else if ($(window).scrollTop() > 8917 && $(window).scrollTop() < 9712){
+					medicalBackground.style.background = 'url(../img/medical-base-2.jpg) no-repeat center top'
+					medicalBackground.style.backgroundAttachment = 'fixed'
+                }
+                else if ($(window).scrollTop() > 9712){
+					medicalBackground.style.background = 'url(../img/medical-base-3.jpg) no-repeat center top'
+					medicalBackground.style.backgroundAttachment = 'fixed'
+				}
+
+        });
+});
 
