21f53df071ee23552bfdaac6f39d3b38afe4a2d7
Zzzoga <105968987+Zzzoga@users.noreply.github.com>
2022-10-21 10:54:09 +0500

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,3 +1,5 @@
+window.onload = function () {
+
 // AOS init
 
 AOS.init();
@@ -23,6 +25,7 @@ SmoothScroll({
 
 
 // SLICK SLIDER ON HEADER
+
 $('.slider').slick({
 	dots: true,
 	infinite: true,
@@ -30,8 +33,81 @@ $('.slider').slick({
 	slidesToShow: 1,
 	arrows: false,
 	autoplay: true,
-	autoplaySpeed: 5000
+	autoplaySpeed: 6000
   });
+
+document.querySelector('#slick-slide-control00').innerHTML = '01'
+document.querySelector('#slick-slide-control01').innerHTML = '02'
+document.querySelector('#slick-slide-control02').innerHTML = '03'
+
+// $('.afisha__list').slick({
+// 	slidesToShow: 5,
+// 	slidesToScroll: 1,
+// 	autoplay: true,
+// 	autoplaySpeed: 2000,
+//   });
+
+// $('.sales__list').slick({
+// 	slidesToShow: 5,
+// 	slidesToScroll: 1,
+// 	autoplay: true,
+// 	autoplaySpeed: 2000,
+//   });
+
+// map icons animated
+
+
+function showHideSticks(icon, stick) {
+	document.querySelector(icon).addEventListener('mouseover', (e) => {
+		e.preventDefault()
+		document.querySelector(stick).style.display = 'block'
+		setTimeout( () => {
+			document.querySelector(stick).style.opacity = '1'
+		}, 200)
+	})
+
+	document.querySelector(icon).addEventListener('mouseout', (e) => {
+			e.preventDefault()
+			document.querySelector(stick).style.opacity = '0'
+			setTimeout( () => {
+				document.querySelector(stick).style.display = 'none'
+			}, 500)
+	})
+}
+
+showHideSticks('.icons img.vosduh', '.stick-1')
+showHideSticks('.icons img.banya', '.stick-2')
+showHideSticks('.icons img.dom-1', '.stick-3')
+showHideSticks('.icons img.dom-2', '.stick-4')
+showHideSticks('.icons img.dom-3', '.stick-5')
+showHideSticks('.icons img.dom-4', '.stick-6')
+showHideSticks('.icons img.dom-5', '.stick-7')
+showHideSticks('.icons img.dom-6', '.stick-8')
+showHideSticks('.icons img.dom-7', '.stick-9')
+showHideSticks('.icons img.domik-1', '.stick-10')
+showHideSticks('.icons img.magazin', '.stick-11')
+showHideSticks('.icons img.lestnica-1', '.stick-12')
+showHideSticks('.icons img.lupa', '.stick-13')
+showHideSticks('.icons img.lestnica-2', '.stick-14')
+showHideSticks('.icons img.venik', '.stick-15')
+showHideSticks('.icons img.serdce', '.stick-16')
+showHideSticks('.icons img.instrumenti', '.stick-17')
+showHideSticks('.icons img.sport', '.stick-18')
+showHideSticks('.icons img.domik-2', '.stick-19')
+
+// Fixed header when scroll
+
+$(function() {
+	let header = $('header');
+	 
+	$(window).scroll(function() {
+	  if($(this).scrollTop() > 1) {
+	   header.addClass('header_fixed');
+	  } else {
+	   header.removeClass('header_fixed');
+	  }
+	});
+   });
 
 // GSAP Animation Full-width block with infoblock
 
@@ -44,17 +120,17 @@ tl.to('.three', {x: '-900px', duration: 5}, 0)
 tl.to('.five', {x: '900px', duration: 5}, 0)
 tl.to('.six', {x: '900px', duration: 5}, 0)
 tl.to('.seven', {x: '1200px', duration: 5}, 0)
-tl.to('.four img', {width: '130vw', top: '-300px', duration: 5}, 0)
+tl.to('.four img', {width: '120vw', top: '-300px', duration: 5}, 0)
 tl.fromTo('.overlay-img', 
 	{width: '800px', height: '641px', opacity: 0, top: '55px', duration: 5}, 
-	{width: '2496px', height: '1999.91px', opacity: 1, top: '-300px', duration: 5}, 
+	{width: '2304px', height: '1846.08px', opacity: 1, top: '-300px', duration: 5}, 
 0)
 
 tl.to('.infoblock-after', {top: '50vh', duration: 10}, 0)
-tl.fromTo('.first-item', {top: '240vh', left: '-600px', duration: 3}, {top: '120vh', left: '200px', duration: 10}, 0)
-tl.fromTo('.second-item', {top: '270vh', left: '-900px', duration: 3}, {top: '220vh', left: '300px',  duration: 15}, 0)
-tl.fromTo('.third-item', {top: '240vh', right: '-600px', duration: 3}, {top: '120vh', right: '300px', duration: 10}, 0)
-tl.fromTo('.fourth-item', {top: '270vh', right: '-900px', duration: 3}, {top: '220vh', right: '200px', duration: 15}, 0)
+tl.fromTo('.first-item', {top: '240vh', left: '-600px', duration: 3}, {top: '120vh', left: '150px', duration: 10}, 0)
+tl.fromTo('.second-item', {top: '270vh', left: '-900px', duration: 3}, {top: '200vh', left: '150px',  duration: 15}, 0)
+tl.fromTo('.third-item', {top: '240vh', right: '-600px', duration: 3}, {top: '135vh', right: '285px', duration: 10}, 0)
+tl.fromTo('.fourth-item', {top: '270vh', right: '-900px', duration: 3}, {top: '215vh', right: '285px', duration: 15}, 0)
 
 // tl.to('.infoblock', {top: '70vh', duration: 3}, 1)
 
@@ -71,22 +147,7 @@ ScrollTrigger.create({
 
 // Fixed title when scroll infoblocks
 
-// const tlTitle = gsap.timeline();
 
-// tlTitle.fromTo('.medical__title', {top: '0', duration: 3}, {top: '130px', duration: 5}, 0)
-// // tlTitle.fromTo('.medical__items', {top: '0', duration: 3}, {top: '-500px', duration: 5}, 0)
-
-
-
-// ScrollTrigger.create({
-// 	animation: tlTitle,
-// 	trigger: '.medical-infoblocks',
-// 	markers: true,
-// 	start: 'top top',
-// 	end: 'bottom bottom',
-// 	scrub: 2,
-// 	pin: true
-// });
 
 // Higher blocks + parallax on scroll action
 
@@ -116,15 +177,15 @@ const medicalBackground = document.querySelector(".medical__inner")
 
 $(document).ready(function(){
     $(window).scroll(function(){
-				if ($(window).scrollTop() > 8019 && $(window).scrollTop() < 8917){
-					medicalBackground.style.background = 'url(img/medical-base-1.png) no-repeat center top'
+				if ($(window).scrollTop() > 7760 && $(window).scrollTop() < 8600){
+					medicalBackground.style.background = 'url(img/lech-1.jpg) no-repeat center top'
 					medicalBackground.style.backgroundAttachment = 'fixed'
-				} else if ($(window).scrollTop() > 8917 && $(window).scrollTop() < 9712){
-					medicalBackground.style.background = 'url(img/medical-base-2.jpg) no-repeat center top'
+				} else if ($(window).scrollTop() > 8601 && $(window).scrollTop() < 9350){
+					medicalBackground.style.background = 'url(img/lech-2.jpg) no-repeat center top'
 					medicalBackground.style.backgroundAttachment = 'fixed'
                 }
-                else if ($(window).scrollTop() > 9712){
-					medicalBackground.style.background = 'url(img/medical-base-3.jpg) no-repeat center top'
+                else if ($(window).scrollTop() > 9350){
+					medicalBackground.style.background = 'url(img/lech-3.jpg) no-repeat center top'
 					medicalBackground.style.backgroundAttachment = 'fixed'
 				}
 
@@ -138,16 +199,12 @@ const cloudRight = document.querySelector(".right-cloud")
 
 $(document).ready(function(){
     $(window).scroll(function(){
-				if ($(window).scrollTop() > 4350 && $(window).scrollTop() < 5800) {
-					cloudLeft.style.transition = `all 1.5s ease;`
-					cloudRight.style.transition = `all 1.5s ease;`
+				if ($(window).scrollTop() > 3845 && $(window).scrollTop() < 5210) {
 					setTimeout(() => {
-						cloudLeft.style.left = `-1050px`
-						cloudRight.style.right = `-1050px`
+						cloudLeft.style.left = `-1500px`
+						cloudRight.style.right = `-1500px`
 					}, 300)
 				} else {
-					cloudLeft.style.transition = `all 1.5s ease;`
-					cloudRight.style.transition = `all 1.5s ease;`
 					cloudLeft.style.left = `0`
 					cloudRight.style.right = `0`
 				}
@@ -207,3 +264,7 @@ afishaBtns.forEach(item => {
 		e.target.parentNode.parentNode.querySelector('.afisha-img-wrap img').style.scale = '1'
 	})
 })
+
+
+}
+
